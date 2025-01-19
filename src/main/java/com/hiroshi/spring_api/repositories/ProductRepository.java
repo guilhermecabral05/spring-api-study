9fd669400f30d3624fc9dd9a76ab13c572aae21b
guilhermecabral05 <guilherme.cabral@sgsistemas.com.br>
2025-01-19 04:14:48 -0300

    fix: lombok
    In Intellij, lombok is not working because the Annotation processors. Changing to "Obtain processors from project classpath" fixed it

--- src/main/java/com/hiroshi/spring_api/repositories/ProductRepository.java
+++ src/main/java/com/hiroshi/spring_api/repositories/ProductRepository.java
@@ -2,7 +2,8 @@ package com.hiroshi.spring_api.repositories;
 
 import com.hiroshi.spring_api.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, String> {
 }
